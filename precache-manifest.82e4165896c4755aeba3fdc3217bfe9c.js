721b1469757ce92c888cba29fa71231dde815967
Virunee <viravongv@gmail.com>
2020-04-28 18:32:05 +0100

    Updates

---
+++ precache-manifest.82e4165896c4755aeba3fdc3217bfe9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62817f12e94c936ba798f2656a82960e",
+    "url": "/react-app-github-pages/index.html"
+  },
+  {
+    "revision": "cc49a10d2964e66f7e88",
+    "url": "/react-app-github-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "15c4ff96d86e8071b866",
+    "url": "/react-app-github-pages/static/js/2.6e9285b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-github-pages/static/js/2.6e9285b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc49a10d2964e66f7e88",
+    "url": "/react-app-github-pages/static/js/main.932a3709.chunk.js"
+  },
+  {
+    "revision": "dd2d0c7bffe6c1b81017",
+    "url": "/react-app-github-pages/static/js/runtime-main.6fc7cb6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
